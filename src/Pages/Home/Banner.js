25469c488aa84a21d0268735752cbcc18028dd15
Mejbah <mejba.s@gmail.com>
2022-08-20 14:35:51 +0600

    stat added

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Banner = () => {
+	return (
+		<div>
+			<div class="hero lg:my-20">
+				<div class="hero-content flex-col lg:flex-row-reverse">
+					<img
+						src="https://i.ibb.co/Cm3pvPd/coding-and-testing-programming-of-software.jpg"
+						class="max-w-sm rounded-lg shadow-2xl"
+					/>
+					<div class="text-center lg:text-left">
+						<h1 class="text-5xl font-bold">You Learn Code?</h1>
+						<p class="py-6">
+            If you want to learn coding and make yourself proficient, then don't delay and start now. Tech Mart has many courses for you. First, set your goals. Then find the course of your choice and start working. thank you
+						</p>
+						<button class="btn btn-primary">Get Started</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Banner;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from './Banner';
+import Stat from './Stat';
 
 const Home = () => {
   return (
     <div>
-      <h2>This is Home</h2>
+      <Banner></Banner>
+      <Stat></Stat>
     </div>
   );
 };
