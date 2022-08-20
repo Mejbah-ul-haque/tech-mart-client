dd0e6c7fe1062cbee5312c4a05f5bfce59817e5f
Mejbah <mejba.s@gmail.com>
2022-08-20 12:56:25 +0600

    footer included

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Footer from './Pages/Shered/Footer';
 import Navbar from './Pages/Shered/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Footer></Footer>
     </div>
   );
 }
