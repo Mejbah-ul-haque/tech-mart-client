71a864469105e4ffdc57c602b13a481ec28f687b
Mejbah <mejba.s@gmail.com>
2022-08-20 13:59:04 +0600

    Login component added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Pages/Shered/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
       </Routes>
       <Footer></Footer>
     </div>
